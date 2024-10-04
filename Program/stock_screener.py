1f8a843ebffd1c5e9b01047f0d2e32d47cbb5e6b
Kelvin Yue <ckkyue@connect.ust.hk>
2024-10-04 18:16:30 +0800

    In progress

--- Program/stock_screener.py
+++ Program/stock_screener.py
@@ -551,7 +551,7 @@ def main():
     backtest = False
 
     # Index
-    index_name = "^GSPC"
+    index_name = "^HSI"
     index_dict = {"^HSI": "HKEX", "^GSPC": "S&P 500", "^IXIC": "NASDAQ Composite"}
 
     # Stock selection
